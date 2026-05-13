6b35a65e7fd34fe15802ee3e4432fc09a45a3c45
Lior Gabay <liorgediting@gmail.com>
2026-05-13 17:13:27 +0300

    feat: add Microsoft Clarity tracking script

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,6 +56,19 @@ export default function RootLayout({
             alt=""
           />
         </noscript>
+        <Script
+          id="clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "wqhz9q56zv");
+            `,
+          }}
+        />
       </head>
       <body className="min-h-full flex flex-col antialiased">{children}</body>
     </html>
